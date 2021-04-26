b997460d29c69f5eecf705946f6ff00da3d0118d
AtaTod <tod.atanas@gmail.com>
2021-04-26 18:16:12 +0100

    First Solution

---
+++ src/main/java/RangeParser.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RangeParser {
+
+    public int[] rangeParser(String tokensInput) {
+        String[] tokens = tokensInput.split(", |,");
+
+        List<Integer> output = new ArrayList<>();
+        for (String token : tokens) {
+            if (token.indexOf(':') != -1) {
+                String[] values = token.split("-|:");
+                for (int i = Integer.valueOf(values[0]); i <= Integer.valueOf(values[1]); i += Integer.valueOf(values[2])) {
+                    output.add(i);
+                }
+            } else if (token.indexOf('-') != -1) {
+                String[] values = token.split("-");
+                for (int i = Integer.valueOf(values[0]); i <= Integer.valueOf(values[1]); i++) {
+                    output.add(i);
+                }
+            } else {
+                output.add(Integer.valueOf(token));
+            }
+        }
+
+        return output.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
