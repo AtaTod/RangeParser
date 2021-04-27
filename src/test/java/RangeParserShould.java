263a5ac4ab1b743a0c858b685495209e4caea04a
AtaTod <tod.atanas@gmail.com>
2021-04-27 09:12:22 +0100

    🔴 (Red) allow_for_single_range_each_factor_token

--- src/test/java/RangeParserShould.java
+++ src/test/java/RangeParserShould.java
@@ -23,4 +23,9 @@ public class RangeParserShould {
     void allow_for_single_range_token() {
         assertArrayEquals(new RangeParser().rangeParser("1-10"), new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
     }
+
+    @Test
+    void allow_for_single_range_each_factor_token() {
+        assertArrayEquals(new RangeParser().rangeParser("1-10:2"), new int[]{1, 3, 5, 7, 9});
+    }
 }
